82c84a09081426422ecc1b46dc3d87978de45606
root <balajimohan80@gmail.com>
2022-08-01 15:33:11 +0000

    Modified slam to add robot initial pose

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 cd $(pwd)/../..;
+catkin_make
 
-xterm -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/map/my_robot/world/maps.world"; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e "source devel/setup.bash;
+	  export ROBOT_INITIAL_POSE='-x 0 -y 0 -z 0 -R 0 -P 0 -Y 0';
+          export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/map/maps.world"; 
+	  roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 10
 
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+xterm -e "source devel/setup.bash;
+	  roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
 
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e "source devel/setup.bash; 
+	  roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
 
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
+xterm -e "source devel/setup.bash; 
+	  roslaunch turtlebot_teleop keyboard_teleop.launch "
 
