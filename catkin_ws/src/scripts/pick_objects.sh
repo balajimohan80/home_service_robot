8ba97cdc60e68c519125fe6a47b6c37bd881e7e2
root <balajimohan80@gmail.com>
2022-08-09 06:56:26 +0000

    Adding pick_obejcts script

---
+++ catkin_ws/src/scripts/pick_objects.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd $(pwd)/../..;
+catkin_make
+
+
+xterm -e "source devel/setup.bash;
+	  export ROBOT_INITIAL_POSE='-x 0 -y 0 -z 0 -R 0 -P 0 -Y 0';
+          export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/map/maps.world"; 
+	  roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 10
+
+xterm  -e "source devel/setup.bash; 
+	   export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/map/maps.yaml";
+	   roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 10
+
+xterm  -e "source devel/setup.bash; 
+	   roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
+sleep 3
+xterm -e "source devel/setup.bash;
+	  rosrun pick_objects pick_objects "
+ 
